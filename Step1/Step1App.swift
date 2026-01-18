594ff32e4877633b069ad87b6935fc56e76d858d
Alex Balakin <ieskelanikalab@gmai.com>
2026-01-18 18:12:44 +0100

    Initial Commit

---
+++ Step1/Step1App.swift
@@ -0,0 +1,17 @@
+//
+//  Step1App.swift
+//  Step1
+//
+//  Created by Alex Balakin on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Step1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
